01a0dd8b621cee1b133a195a6c27bf83ead4eb1c
kevops <crawfordk2.0@gmail.com>
2024-07-02 09:50:44 -0400

    added metrics

--- forecast.go
+++ forecast.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -25,6 +26,15 @@ type WeatherData struct {
 	Wind struct {
 		Speed float64 `json:"speed"`
 	}
+}
+
+type CityMetrics struct {
+	City      string
+	Humidity  float64
+	Temp      int
+	Low       int
+	High      int
+	WindSpeed float64
 }
 
 func UnmarshalJSON[T any](data []byte) (T, error) {
@@ -56,6 +66,48 @@ func GetWeatherData(url string) (WeatherData, error) {
 	return weatherData, nil
 }
 
+func sendMetrics(citiesMetrics []CityMetrics) error {
+	var data string
+	for _, cm := range citiesMetrics {
+		data += fmt.Sprintf(
+			"relative_humidity{city=\"%s\"} %.2f\n"+
+				"current_temperature{city=\"%s\"} %d\n"+
+				"low_temp{city=\"%s\"} %d\n"+
+				"high_temp{city=\"%s\"} %d\n"+
+				"wind_speed{city=\"%s\"} %.2f\n",
+			cm.City, cm.Humidity, cm.City, cm.Temp, cm.City, cm.Low, cm.City, cm.High, cm.City, cm.WindSpeed,
+		)
+	}
+	// Print metrics
+	fmt.Println("Metrics being sent:\n", data)
+
+	// Endpoint where the metrics will be sent
+	url := "http://your.monitoring.tool/metrics"
+
+	// Create a new POST request with the formatted data
+	req, err := http.NewRequest("POST", url, bytes.NewBufferString(data))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "text/plain")
+
+	// Send the request using the http client
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Check if the POST request was successful
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to send metrics, status code: %d", resp.StatusCode)
+	}
+
+	fmt.Println("Metrics successfully sent")
+	return nil
+}
+
 func main() {
 	urls := make(map[string]string)
 	OW_KEY := os.Getenv("OW_KEY")
@@ -65,6 +117,8 @@ func main() {
 	urls["New York"] = "https://api.openweathermap.org/data/2.5/weather?lat=40.7833&lon=-73.9666&units=imperial&appid=" + OW_KEY
 	urls["Chicago"] = "https://api.openweathermap.org/data/2.5/weather?lat=41.8376&lon=-87.6818&units=imperial&appid=" + OW_KEY
 
+	var citiesMetrics []CityMetrics
+
 	for city, url := range urls {
 		WeatherData, err := GetWeatherData(url)
 		if err != nil {
@@ -72,14 +126,26 @@ func main() {
 			continue
 		}
 
-		humidity := WeatherData.Main.Humidity
-		temp := WeatherData.Main.Temp
-		low := WeatherData.Main.Low
-		wind_speed := WeatherData.Wind.Speed
+		cm := CityMetrics{
+			City:      city,
+			Humidity:  WeatherData.Main.Humidity,
+			Temp:      int(WeatherData.Main.Temp),
+			Low:       int(WeatherData.Main.Low),
+			High:      int(WeatherData.Main.High),
+			WindSpeed: WeatherData.Wind.Speed,
+		}
 
-		fmt.Printf("The current humidity for the area of %s is %.2f%%\n", city, humidity)
-		fmt.Printf("The current temperature for the area of %s is %d degrees\n", city, int(temp))
-		fmt.Printf("The current low temperature for the area of %s is %d degrees\n", city, int(low))
-		fmt.Printf("The current wind speed for the area of %s is %.2f mph\n", city, wind_speed)
+		citiesMetrics = append(citiesMetrics, cm)
+
+		fmt.Printf("The current humidity for the area of %s is %.2f%%\n", city, cm.Humidity)
+		fmt.Printf("The current temperature for the area of %s is %d degrees\n", city, cm.Temp)
+		fmt.Printf("The current low temperature for the area of %s is %d degrees\n", city, cm.Low)
+		fmt.Printf("The current high temperature for the area of %s is %d degrees\n", city, cm.High)
+		fmt.Printf("The current wind speed for the area of %s is %.2f mph\n", city, cm.WindSpeed)
+	}
+
+	err := sendMetrics(citiesMetrics)
+	if err != nil {
+		fmt.Printf("Failed to send metrics: %v\n", err)
 	}
 }
